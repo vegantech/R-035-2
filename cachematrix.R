f5fcb10a2fa5ec1dc496fc3233dd713efa5157ee
Shawn Balestracci <shawn@vegantech.com>
2015-12-16 21:57:57 -0800

    implement without caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R (rprog-35) Assignment 2 Caching and Inverse of Matrix
+## Estimated score:  7 out of 12
+## 1 valid repo
+## 1 >1 commit
+## 1 sha-1 submitted
+## 1 corresponding commit
+## 1 partially complete  [out of 2]
+## 2 functions are commented
+## 0 solution is not correct [out of 2]
+## 0 solution is not correct [out of 2]
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  ### Creates a special "matrix" object that can cache its inverse.
+  ### x is a matrix()
+  ### returns a cachable matrix that can be used with cacheSolve
+  
+  
+  ### for now it just returns the original matrix
+  x
 
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  ## x is a cachable matrix created previously by makeCacheMatrix
+  
+  ### solve calculates the inverse of a matrix,  pass in remaining arguments
+  solve(x, ...)     
 }
